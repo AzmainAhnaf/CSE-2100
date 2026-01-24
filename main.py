cc0dd0a6de94a49cf4a7b6393bf7e76c51cf4438
Azmain Ahnaf <87205879+AzmainAhnaf@users.noreply.github.com>
2026-01-24 12:08:33 +0600

    Filtering submission based on accepted and wrong verdict.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_user(handle):
     return data
 
 # Check Max 'lmt' latest submission of the user 'handle'
-# Return list of AC Submission
+# Return list of AC Submission of unique problems
 def get_submissions(handle, lmt):
     url = base_url + "user.status?handle=" + handle + "&from=1&count=" + str(lmt)
     response = requests.get(url)
@@ -48,6 +48,12 @@ def get_submissions(handle, lmt):
             ndata.append(item)
     return ndata
 
+def get_all_submissions(handle, lmt):
+    url = base_url + "user.status?handle=" + handle + "&from=1&count=" + str(lmt)
+    response = requests.get(url)
+    data = response.json()
+    return data["result"]
+
 
 def main():
     # Taking input handle
@@ -56,12 +62,17 @@ def main():
 
     # Checking existence of User. If found fetching userbase around user's rating
     data = get_user(handle)
+
+    # Stores relevant userbase
     users = list()
+
+    # Stores rating of the user if found
+    rating = 0
     if (data["status"] == "OK"):
         print("Handle Found")
         rating = data["result"][0]["rating"]
         print(f"Fetching Random Handles ranging [{rating - 100}, {rating + 100}]")
-        users = get_userbase(rating - 100, rating + 100, 1000)
+        users = get_userbase(rating - 100, rating + 100, 10)
     else:
         print(data["comment"])
         return -1
@@ -73,7 +84,7 @@ def main():
     tagcount = dict()
     for user in users:
         print("Processing", user[0], ", Rating: ", user[1])
-        submission = get_submissions(user[0], 500)
+        submission = get_submissions(user[0], 10)
         for item in submission:
             for tag in item["problem"]["tags"]:
                 tc += 1
@@ -86,11 +97,49 @@ def main():
     for key in tagcount:
         print(key, tagcount[key])
  
+    count = 0 # Count of total considered submission
     total = dict() # Store count of total submission of a specific tag
     bad = dict()  # Store count of bad submission of a specific tag
     good = dict() # Store count of good submission of a specific tag
 
+    submission = get_all_submissions(handle, 500)
 
+    # Filtering out submission based on accepted, and wrong verdict
+    for item in submission:
+        verdict = item["verdict"]
+        if verdict == "COMPILATION_ERROR":
+            continue
+        if "rating" not in item["problem"]:
+            continue
+        if item["problem"]["rating"] < rating - 150:
+            continue
+        if verdict == "OK":
+            for tag in item["problem"]["tags"]:
+                if tag in good:
+                    good[tag] += 1
+                else:
+                    good[tag] = 1
+                if tag in total:
+                    total[tag] += 1
+                else:
+                    total[tag] = 1
+                if tag not in bad:
+                    bad[tag] = 0
+        else:
+            for tag in item["problem"]["tags"]:
+                if tag in good:
+                    bad[tag] += 1
+                else:
+                    bad[tag] = 1
+                if tag in total:
+                    total[tag] += 1
+                else:
+                    total[tag] = 1
+                if tag not in good:
+                    good[tag] = 0
+
+    for key in total:
+        print(f"{key} Total = {total[key]}, Good = {good[key]}, Bad = {bad[key]}")
     
 
 if __name__ == "__main__":
